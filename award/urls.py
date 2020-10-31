06588a887fe1b6ac6afeae803f2d31fee0e49624
Bernice2001 <wangechibanim@gmail.com>
2020-10-31 15:20:55 +0300

    add post

--- award/urls.py
+++ award/urls.py
@@ -10,6 +10,7 @@ urlpatterns=[
     path('add_new_project/', views.post_project, name='addProject'),
     path('<uuid:post_id>', views.single_project, name='post'),
     path('<uuid:post_id>/like', views.like, name='likePost'),
+    path('search/', views.search_results, name='search'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
     path('accounts/login', views.signin, name='login'),
