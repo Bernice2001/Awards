e84857fb7f7a04c35b1e152907b173a1f060f2c6
Bernice2001 <wangechibanim@gmail.com>
2020-11-01 19:12:45 +0300

    add profile test case

--- award/tests.py
+++ award/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='testusereres', password='12345')
+        
+        self.profile = Profile(user=user, location='Kenya', bio='me..me', avatar='image.jpg')
+                
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
